f022fbfc4da4760603f42262d65a5201dfb2a720
Rajasekar Kalisamy <rajashekarkalisamy@gmail.com>
2025-10-05 03:33:44 +0530

    refresh api integration completed

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,76 +2,73 @@ import { Injectable } from '@angular/core';
 import { CommonService } from '../common.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private loggedIn: boolean = false;
-  private userRole: string = '';
-  private userEmail: string = '';
-  public authToken: string = '';
 
-  constructor(private commonService: CommonService) { }
+  constructor(private commonService: CommonService) {}
 
   login(data: any) {
-    this.commonService.request("users/login", "POST", data).then((response: any) => {
-      if (response.success) {
-        if (response.statusCode == "R200" && response.data && response.data.jwt) {
-          this.authToken = response.data.jwt;
-          this.loggedIn = true
-          localStorage.setItem('auth_token', this.authToken);
-          if (data.remember_device) {
-            localStorage.setItem('email', data.email);
-            localStorage.setItem('password', data.password);
-            localStorage.setItem('remember_device', data.remember_device);
-          } else {
-            localStorage.removeItem('email');
-            localStorage.removeItem('password');
-            localStorage.removeItem('remember_device');
+    this.commonService
+      .request('users/login', 'POST', data)
+      .then((response: any) => {
+        if (response.success) {
+          if (
+            response.statusCode == 'R200' &&
+            response.data &&
+            response.data.jwt
+          ) {
+            localStorage.setItem('auth_token', response.data.jwt);
+            if (data.remember_device) {
+              localStorage.setItem('email', data.email);
+              localStorage.setItem('password', data.password);
+              localStorage.setItem('remember_device', data.remember_device);
+            } else {
+              localStorage.removeItem('email');
+              localStorage.removeItem('password');
+              localStorage.removeItem('remember_device');
+            }
+            window.location.reload();
           }
-          this.commonService.redirect('/dashboard')
         }
-      }
-    }).catch((error) => { });
+      })
+      .catch((error) => {});
   }
-  verifyToken = () => {
-    const data = {
-      "token": this.getToken()
-    }
 
-    return this.commonService.request("users/verify", "POST", data).then((response: any) => {
-      if (response.success) {
-        if (response.statusCode == "R200" && response.data && response.data.email) {
-          this.userEmail = response.data.email;
-          this.loggedIn = true
-        }else{
-          this.logout();
-          this.commonService.redirect("/authentication/login");
-        }
-      } else {
-        this.logout();
-        this.commonService.redirect("/authentication/login");
-      }
-    }).catch(error => {
-      this.logout();
-      this.commonService.redirect("/authentication/login");
+  refreshToken = (): Promise<any> => {
+    return this.commonService.request(
+      'users/refresh',
+      'POST',
+      {},
+      { withCredentials: true }
+    );
+  };
+
+  verifyToken = () => {
+    return this.commonService.request('verify-login', 'GET').then(() => {
+      this.setLoggedIn(true);
     });
-  }
+  };
 
   logout(): void {
-    this.loggedIn = false;
-    this.userRole = '';
+    this.setLoggedIn(false);
     localStorage.removeItem('auth_token');
+    this.commonService.redirect('/authentication/login');
   }
 
   isLoggedIn(): boolean {
     return this.loggedIn;
   }
 
-  getUserRole(): string {
-    return this.userRole;
+  setLoggedIn(isLoggedIn: boolean) {
+    this.loggedIn = isLoggedIn;
   }
+
   getToken(): string | null {
     return localStorage.getItem('auth_token');
+  }
+  storeAccessToken(token: string) {
+    localStorage.setItem('auth_token', token);
   }
 }
