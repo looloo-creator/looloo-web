a332930a08d3274874f3909d1f3b25d38f3ea8a2
Rajasekar Kalisamy <rajashekarkalisamy@gmail.com>
2026-03-14 03:17:11 +0530

    feat:google signin updated

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { environment } from 'src/environments/environment';
+
+declare const google: any;
 
 @Component({
     selector: 'app-login',
@@ -8,40 +10,32 @@ import { AuthService } from 'src/app/services/auth/auth.service';
     standalone: false
 })
 export class AppSideLoginComponent {
-  loginFormSubmitted: boolean = false;
-  rememberDevice: boolean = false;
-  email: string | null = "";
-  password: string | null = "";
+  constructor(private authService: AuthService) {}
 
-  constructor(private authService: AuthService) {
-    this.rememberDevice = localStorage.getItem('remember_device') ? true : false;
-    if (this.rememberDevice) this.email = localStorage.getItem('email'); this.password = localStorage.getItem('password');
-    this.loginForm.patchValue({
-      email : this.email,
-      password : this.password
+  private ensureGoogleScriptLoaded(): Promise<void> {
+    return new Promise((resolve) => {
+      if ((window as any).google && (window as any).google.accounts) {
+        return resolve();
+      }
+      const script = document.createElement('script');
+      script.src = 'https://accounts.google.com/gsi/client';
+      script.async = true;
+      script.defer = true;
+      script.onload = () => resolve();
+      document.head.appendChild(script);
     });
   }
-  loginForm = new FormGroup({
-    email: new FormControl(this.email, [Validators.required, Validators.pattern("[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}")]),
-    password: new FormControl(this.password, [Validators.required, Validators.minLength(6)]),
-  });
 
-  get lf() {
-    return this.loginForm.controls;
-  }
-
-  login = () => {
-    this.loginFormSubmitted = true;
-    if (this.loginForm.valid) {
-      let requestData = {
-        "email": this.lf.email.value,
-        "password": this.lf.password.value,
-        "remember_device": this.rememberDevice
-      }
-      this.authService.login(requestData);
-    }
-  }
-  toggle(value: boolean) {
-    this.rememberDevice = value;
+  async loginWithGoogle() {
+    await this.ensureGoogleScriptLoaded();
+    google.accounts.id.initialize({
+      client_id: environment.googleClientId,
+      callback: (response: any) => {
+        if (response?.credential) {
+          this.authService.socialLogin(response.credential);
+        }
+      },
+    });
+    google.accounts.id.prompt();
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    apiDomain: "http://localhost:30000/"
+    apiDomain: "http://localhost:30000/",
+    googleClientId: "1094352326643-997i1qnqr8sl3j6hae0mg1fp7kn0j0gp.apps.googleusercontent.com",
 };
